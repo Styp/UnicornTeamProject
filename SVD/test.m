b3eeeeb28e20a269eb424976aac1900f09bb99a5
Martin Stypinski <mstypniski@gmail.com>
2017-04-06 16:15:20 +0200

    first attempt SVD

---
+++ SVD/test.m
@@ -0,0 +1,49 @@
+testImg = imread('/opt/dataset/GTSRB/Final_Training/Images/00014/00000_00000.ppm');
+
+grayImg = rgb2gray(testImg);
+
+figure
+grayImg
+imshow(grayImg)
+
+%%% compression
+[TU,TS,TV] = svd(single(grayImg));
+SV = diag(S)
+s = size(SV);
+
+rank = s(1);
+
+U_compressed = U(:, 1:45);
+V_compressed = V(1:45, :);
+
+vector = 1:45
+
+TCS(1) = SV(1);
+for k = 2: s(1)
+    TCS(k) = TCS(k-1) + SV(k);
+end
+
+
+%%% decompression
+v = 2
+
+% find the rank used for each block
+rr = 1;
+k = s(1)-1;
+while (k >=1) 
+        if ( (TCS(k) <= v) & (TCS(k+1) >= v))
+                rr = k+1;
+                break;
+        end;
+        k = k-1;
+end
+
+  X = TU(:, 1:rr) * TS(1:rr, 1:rr) * TV(:, 1:rr)'
+  
+  figure
+  imshow(uint8(X));
+  
+  figure
+  testicles = uint8(grayImg) - uint8(X);
+  imshow(uint8(testicles));
+  
