e9d354b4122772af22a70d9212d0345d502bd7e4
CaroHeil <caroline.heil@live.de>
2017-04-21 17:30:01 +0200

    Add files via upload

--- Caroline/Code/Test.m
+++ Caroline/Code/Test.m
@@ -1,8 +1,15 @@
-for i = 1:24
+for i = 1:10
 
- string = ['Testbilder/Test',num2str(i),'.jpg'] ;
+ string = ['TestImages/GiveWay/Test',num2str(i),'.jpg'] ;
 
  image = imread(string);
+ 
+ im_size = size(image);
+  
+ %resize = 400/im_size(1);
+ 
+ %image = imresize(image,resize);
+ 
  %bw= rgb2gray(image);
  %bw = edge(bw, 'Canny',[0.01, 0.5]);
  %bw = personalized_edge_detection(image);
@@ -16,14 +23,11 @@ for i = 1:24
 %version 1 vs. version 2 
 
 
-boxes = detect_signs(image);
+[boxes] = detect_signs(image);
+
+if isempty(boxes) == false
 figure 
 imshow(image)
 plot_bounding_boxes(boxes,200)
-
-boxes = detect_signs_2(image);
-figure 
-imshow(image)
-plot_bounding_boxes(boxes,200
-)
+end
 end
